39b108ca659c9b0ff6de91ba5707f9d2e97fa49c
Oluwasegun122 <ooluwasegunismaheel@gmail.com>
2024-11-23 20:34:25 +0100

    first to github

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,16 @@ export default function App() {
   const [foodData, setFoodData] = useState([]);
   const [foodId, setFoodId] = useState("656329");
   return (
-    <div className="hidden md:block">
-      <Header foodData={foodData} setFoodData={setFoodData} />
-      <FoodList foodId={foodId} setFoodId={setFoodId} foodData={foodData} />
+    <div>
+      <div className="h md:hidden ">
+        <h1 className="text-5xl">
+          To Have Acces To This FoodApp, Please Use Your PC
+        </h1>
+      </div>
+      <div className="hidden md:block">
+        <Header foodData={foodData} setFoodData={setFoodData} />
+        <FoodList foodId={foodId} setFoodId={setFoodId} foodData={foodData} />
+      </div>
     </div>
   );
 }
